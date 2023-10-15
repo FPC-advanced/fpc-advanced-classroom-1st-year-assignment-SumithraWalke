2fc0d717ffe4bf471721385c59c1d4b289458c78
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-10-15 16:56:21 +0530

    committed 10/15/2023, 4:56:20 PM

---
+++ set01/problem07.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input_n()
+{
+    int n;
+    printf("enter the postive integer :");
+    scanf("%d",&n);
+    return n;
+}
+int sum_n_nos(int n)
+{
+    int sum=0;
+    for(int i=0;i<=n;i++)
+    {
+        sum +=i;
+
+    }
+    return sum;
+}
+
+void output(int n,int sum)
+{
+  printf("the sum of numbers from 1 to %d is %d ",n,sum);  
+}
+int main()
+{
+    int n,sum;
+    n=input_n();
+    sum=sum_n_nos(n);
+    output(n,sum);
+    return 0;
+}
