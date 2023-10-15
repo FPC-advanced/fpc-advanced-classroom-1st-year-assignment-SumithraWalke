8d1d193f7840133f3df45983352ea4887c75cff8
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-10-15 17:09:01 +0530

    committed 10/15/2023, 5:09:00 PM

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,12 +1,30 @@
 #include<stdio.h>
 int input(int *a, int *b, int *c)
-{
-   printf("Enter three numbers: ");
+ {
+    printf("Enter three numbers: ");
     scanf("%d %d %d", a, b, c);
     return 0;
 }
-void compare(int a,int b,int c,int *largest)
+void compare(int a, int b, int c, int *largest) 
 {
+    *largest = a; 
+    if (b > *largest) {
+        *largest = b;
+    }
 
-    
+    if (c > *largest) {
+        *largest = c;
+    }
+}
+void output(int a, int b, int c, int largest)
+{
+    printf("the largest among %d,%d,%d is %d",a,b,c,largest);
+}
+int main()
+{
+    int a,b,c,largest;
+    input(&a,&b,&c);
+    compare(a,b,c,&largest);
+    output(a,b,c,largest);
+    return 0;
 }
