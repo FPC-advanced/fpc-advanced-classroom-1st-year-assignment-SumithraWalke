8be118a76bb2caa0f1517b017c0eb7dfc240b11a
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-10-15 16:48:09 +0530

    committed 10/15/2023, 4:48:09 PM

---
+++ set01/problem06.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int input(int *a, int *b, int *c)
+{
+   printf("Enter three numbers: ");
+    scanf("%d %d %d", a, b, c);
+    return 0;
+}
+void compare(int a,int b,int c,int *largest)
+{
+
+    
+}
