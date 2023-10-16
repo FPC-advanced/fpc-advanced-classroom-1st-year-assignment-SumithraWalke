22b3bbfda38bd2837a9ea801d2fd0237fc272f54
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-10-16 09:56:05 +0530

    committed 10/16/2023, 9:56:05 AM

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,10 +1,43 @@
-#include<stdio.h>
-#include<math.h>
-float input()
-{
+#include <stdio.h>
+#include <math.h>
+
+float input() {
     float n;
-    printf("Enter number: ");
-    scanf("%f",&n);
+    printf("Enter a number: ");
+    scanf("%f", &n);
     return n;
 }
-float square_root(float n)
+
+
+float square_root(float n) {
+    if (n < 0) {
+        printf("Square root of a negative number is not defined.\n");
+        return -1.0;
+    }
+    
+    float guess = n; 
+    float previous_guess;
+    float epsilon = 0.0001; 
+
+    do {
+        previous_guess = guess;
+        guess = 0.5 * (guess + n / guess);
+    } while (fabs(guess - previous_guess) > epsilon);
+
+    return guess;
+}
+
+
+void output(float n, float sqrroot) {
+    if (sqrroot >= 0) {
+        printf("The square root of %.2f is approximately %.4f\n", n, sqrroot);
+    }
+}
+
+int main() {
+    float number, result;
+    number = input();
+    result = square_root(number);
+    output(number, result);
+    return 0;
+}
---
+++ set02/problem04.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int input_array_size();
+{
+    int n;
+    printf("enter the size of array:");
+    scanf("%d",&n);
+    return n;
+}
