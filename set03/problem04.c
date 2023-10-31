117a6686d0c89a8149a934bdeb33d7f2dba9e7af
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-10-31 11:50:32 +0530

    committed 10/31/2023, 11:50:31 AM

---
+++ set03/problem04.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int input()
+{
+    int n;
+    printf("Enter the nth number:/n");
+    scanf("%d",&n);
+    return 0;
+}
+int find_fibo(int n)
+{
+   int f0=0,f1=1,f;
+   if(n==0)
+   {
+    return f;
+   } 
+   for(int i=2;i<=n;i++)
+   {
+    f1=f
+   }
+   return f1
+}
