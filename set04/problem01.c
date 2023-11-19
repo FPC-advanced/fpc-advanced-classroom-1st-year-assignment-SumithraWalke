bb937029fb34126f882011fa590b148ebda244b3
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-11-20 00:22:30 +0530

    committed 11/20/2023, 12:22:29 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -8,5 +8,6 @@ void input(int *num1, int *den1, int *num2, int *den2)
 }
 void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
 {
-    *res_num=(num1*den2)+(num2)
+    *res_num=(num1*den2)+(num2*den1);
+    *res_den=(den1*den2);;
 }
