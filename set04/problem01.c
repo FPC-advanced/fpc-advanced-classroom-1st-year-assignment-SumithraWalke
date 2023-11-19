7d3cf27ecb1707e1e72427175e5763fde89fe575
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-11-20 00:17:00 +0530

    committed 11/20/2023, 12:16:59 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -4,5 +4,5 @@ void input(int *num1, int *den1, int *num2, int *den2)
     printf("Enter the first fraction:\n");
     scanf("%d%d",num1,den1);
     printf("Enter the first fraction:\n");
-
+    scanf("%d%d",num2,den1);
 }
