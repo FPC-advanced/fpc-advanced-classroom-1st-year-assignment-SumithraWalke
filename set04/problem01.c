6d560dfb5910036b4f12f71406df7d327b33dd40
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-11-20 00:18:00 +0530

    committed 11/20/2023, 12:17:59 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -3,6 +3,6 @@ void input(int *num1, int *den1, int *num2, int *den2)
 {
     printf("Enter the first fraction:\n");
     scanf("%d%d",num1,den1);
-    printf("Enter the se fraction:\n");
+    printf("Enter the second fraction:\n");
     scanf("%d%d",num2,den2);
 }
