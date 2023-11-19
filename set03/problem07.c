fdc7c5cf17be328ffdaf80171bd789ad5a2e20f6
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-11-20 00:09:30 +0530

    committed 11/20/2023, 12:09:29 AM

---
+++ set03/problem07.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+typedef struct point {
+    float x, y;
+} Point;
+
+typedef struct line {
+    Point p1, p2;
+    float distance;
+} Line;
+Point input_point()
+{
+    printf("enter the po")
+}
