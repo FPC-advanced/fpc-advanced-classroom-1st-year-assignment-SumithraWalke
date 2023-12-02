89298dbf825225f92366aaf9956a7c3053baa6a6
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2023-12-02 22:18:30 +0530

    committed 12/2/2023, 10:18:29 PM

--- set04/problem02.c
+++ set04/problem02.c
@@ -15,5 +15,9 @@ Fraction smallest(Fraction a,Fraction b,fraction c)
     float p,q,r;
     p=a.num/a.den;
     q=b.num/b.den;
-    r=c.num/c.den
+    r=c.num/c.den;
+    if(p<q&&p<r)
+    {
+        return a;
+    }
 }
